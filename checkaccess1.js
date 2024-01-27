0a4e6ed41d5bd314fbb2dc93d8095bdcdfc9514e
jinalipatel <pateljinali2703@gmail.com>
2024-01-27 12:46:00 +0530

    PATEL JINALI

---
+++ checkaccess1.js
@@ -0,0 +1,13 @@
+export default function TodoList() {
+    return (
+      <>
+        <h1>PATEL JINALI</h1>
+        <ul>
+          <li>Invent new traffic lights</li>
+          <li>Rehearse a movie scene</li>
+          <li>Improve the spectrum technology</li>
+        </ul>
+      </>
+    );
+  }
+  
